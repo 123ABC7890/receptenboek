ffa7b5c9b7d3fd1f12cb7d028d511b7abf190c08
Marijn Bary <186516@novacollege.nl>
2023-03-29 13:21:53 +0200

    korst, makkelijk, meeste pagina aangemaakt

--- specials.php
+++ specials.php
@@ -1,12 +1,18 @@
 <?php
-    
+
     require "database.php";
 
-    $sql = "SELECT * FROM canadese_recepten";
+    $sql1 = "SELECT * FROM canadese_recepten  WHERE duur = (SELECT MIN(duur) FROM canadese_recepten)";
+    $result = mysqli_query($conn,$sql1);
+    $all_recepten_kortst_duurt = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    $result = mysqli_query($conn,$sql);
+    $sql2 = "SELECT * FROM canadese_recepten WHERE moeilijkheidsgraad = 'Makkelijk te bereiden'";
+    $result = mysqli_query($conn,$sql2);
+    $all_recepten_Makkelijk = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-    $recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $sql3 = "SELECT * FROM canadese_recepten  WHERE hoeveelheid_ing = (SELECT MAX(hoeveelheid_ing) FROM canadese_recepten)";
+    $result = mysqli_query($conn,$sql3);
+    $all_recepten_Meeste_ingredienten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 ?>
 
@@ -25,8 +31,44 @@
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
-
+    
     <?php include "header.php" ?>
+    
+    <h1>Snelle recepten</h1>
+    <row>
+    <?php foreach($all_recepten_kortst_duurt as $recept): ?>
+
+        <div class="container">
+            <a href="recept.php?id=<?php echo $recept["id"] ?>"><?php echo $recept["naam"] ?></a>
+            <img src="<?php echo $recept["foto"] ?>" alt="no foto">
+        </div>
+
+    <?php endforeach ?>
+    </row>
+
+    <h1>Makkelijke recepten</h1>
+    <row>
+    <?php foreach($all_recepten_Makkelijk as $recept): ?>
+
+        <div class="container">
+            <a href="recept.php?id=<?php echo $recept["id"] ?>"><?php echo $recept["naam"] ?></a>
+            <img src="<?php echo $recept["foto"] ?>" alt="no foto">
+        </div>
+
+    <?php endforeach ?>
+    </row>
+    
+    <h1>Meeste ingredienten recepten</h1>
+    <row>
+    <?php foreach($all_recepten_Meeste_ingredienten as $recept): ?>
+
+        <div class="container">
+            <a href="recept.php?id=<?php echo $recept["id"] ?>"><?php echo $recept["naam"] ?></a>
+            <img src="<?php echo $recept["foto"] ?>" alt="no foto">
+        </div>
+
+    <?php endforeach ?>
+    </row>
     
 </body>
 </html>
